d6a0097e010a734197c1423749711196c89a0890
amora7741 <mora7741@gmail.com>
2024-06-04 15:10:54 -0700

    Layout adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ import { navItems } from '@/data';
 
 export default function Home() {
   return (
-    <main className='min-h-[200vh] relative flex flex-col items-center px-4'>
-      <FloatingNav navItems={navItems} />
+    <main className='min-h-[200vh] relative flex flex-col items-center px-4 overflow-hidden'>
+      <FloatingNav navItems={navItems} className='top-28' />
       <Hero />
+      <div>
+        <h1>Hello</h1>
+      </div>
     </main>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
 const Hero = () => {
   return (
-    <div className='my-20 text-center'>
-      <h1>Hello There</h1>
-      <h2>This is my hero</h2>
+    <div className='pt-20 p-4 text-center w-full h-screen xl:h-[80vh] flex items-center justify-center'>
+      <div className='my-20 flex flex-col gap-6 flex-1'>
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 };
